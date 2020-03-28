1d754db391377f4e7a39334b2ff02c24c623d1e8
Jack Cole <jackcole@umd.edu>
2020-03-28 02:39:56 -0400

    Added cd command to cron job

--- ingest-covid19-normalized.sh
+++ ingest-covid19-normalized.sh
@@ -1,6 +1,7 @@
 # Crude script that pulls the csv and replaces the old one
 # Runs daily as a cron job
 
+cd ~/PycharmProjects/COVID-19-Flask/
 wget -r -A.csv https://data.beta.nyc/dataset/covid-19-time-series/resource/3d4caf81-7ec0-4112-9700-62ca7364d6bf
 mv data.beta.nyc/dataset/ba0dfde7-3a58-4baa-930f-9f27b25b678c/resource/3d4caf81-7ec0-4112-9700-62ca7364d6bf/download/covid19_normalized.csv covid19_normalized.csv
 rm -rf data.beta.nyc
