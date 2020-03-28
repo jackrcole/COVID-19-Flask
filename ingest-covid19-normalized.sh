fb9187045edf34f4237e9c558fae658197019e17
Jack Cole <jackcole@umd.edu>
2020-03-28 02:10:08 -0400

    Changed source of data

---
+++ ingest-covid19-normalized.sh
@@ -0,0 +1,3 @@
+wget -r -A.csv https://data.beta.nyc/dataset/covid-19-time-series/resource/3d4caf81-7ec0-4112-9700-62ca7364d6bf
+mv data.beta.nyc/dataset/ba0dfde7-3a58-4baa-930f-9f27b25b678c/resource/3d4caf81-7ec0-4112-9700-62ca7364d6bf/download/covid19_normalized.csv covid19_normalized.csv
+rm -rf data.beta.nyc
